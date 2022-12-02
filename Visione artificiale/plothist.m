1bd640fbfc775d7e833a9a00269c2812015f6f0a
Francesco Amico <francesco0501@libero.it>
2022-12-02 12:12:20 +0100

    Update plothist.m

--- Visione artificiale/plothist.m
+++ Visione artificiale/plothist.m
@@ -1,6 +1,6 @@
 function h=plothist(img)
     h=imhist(uint8(img),256)';
-    m=101-ceil(100*double(h)/double(max(h(:)))); % normalizzo e trovo la riga di partenza tra 1 e 100
+    m=101-ceil(100*double(h)/double(max(h(:)))); % normalizzo l'istogramma rispetto all'outlier. L'outlier partirà dalla prima riga
     out=zeros(120,256,'uint8')+255;
     for i=1:256
         out(m(i):100,i)=0;
